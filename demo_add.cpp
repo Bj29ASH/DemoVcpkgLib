fca8376a120e7c298d7329b31acd560f55c28cec
akash <akash@launchtrax.com>
2024-09-04 11:57:41 +0530

    Initial commit

---
+++ demo_add.cpp
@@ -0,0 +1,11 @@
+//
+// Created by akash on 04-09-2024.
+//
+
+#include "demo_add.h"
+
+namespace demo {
+int add(int a, int b) {
+  return a + b;
+}
+}
---
+++ demo_add.h
@@ -0,0 +1,12 @@
+//
+// Created by akash on 04-09-2024.
+//
+
+#ifndef DEMO_ADD_H
+#define DEMO_ADD_H
+
+namespace demo {
+int add(int a, int b);
+}
+
+#endif //DEMO_ADD_H
